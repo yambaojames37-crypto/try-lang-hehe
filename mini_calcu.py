08f9f562fe389683e0b4644582e574c07fe5f08e
Cullado, James Carlos Y. <yambaojames37@gmail.com>
2026-08-14 21:33:27 +0800

    Add files via upload

---
+++ mini_calcu.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = str(input("Enter the opertor(+, -, *, /): "))
+
+if c == "+":
+    print(a+b)
+
+elif c == "-":
+    print(a-b)
+
+elif c == "*":
+    print(a*b)
+
+elif c == "/":
+    print(a/b)
+    
+else:
+    print(" ENGKK_ERROR ")
